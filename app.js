80318b61a44c4a7ff665bcc79124fba9a6807f7e
Brett Connell <dev@bretta.io>
2023-09-26 14:25:13 -0400

    allsorts going on - have worked on the app.js - will push and see

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var admin = require('firebase-admin');
+
+var serviceAccount = require("./gcp-bretta-backend-express-key.json");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//Access Firebase Config Variables
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+const app = initializeApp(firebaseConfig);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +36,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 module.exports = app;
